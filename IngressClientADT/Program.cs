920baab1f29d68a793b8d94eebf4676984513761
Barnes-Charles <barnes_charles@bah.com>
2021-07-18 12:08:28 -0500

    project files

---
+++ IngressClientADT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.DigitalTwins.Core;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.FlightSimulator.SimConnect;
+using System.Runtime.InteropServices;
+
+namespace IngressClientADT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingress Client for Microsoft Flight Simulator for ADT Instance");
+            Console.ReadLine();
+        }
+    }
+}
